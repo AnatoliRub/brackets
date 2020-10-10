3fa1ca8e5ede81d864ef0a5f91f51816d2c3c3ba
Anatoli Rubankou <catok1992@gmail.com>
2020-10-10 04:24:46 +0300

    feat: add solution for 'brackets' task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let stack = []
+	let bracket = str.split('')
+	let bracketsPairs = new Object()
+
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		bracketsPairs[`${bracketsConfig[i][0]}`] = `${bracketsConfig[i][1]}`
+	}
+
+	for (let i = 0; i < bracket.length; i++) {
+		let checkBracket = true
+		if (stack.length > 0) {
+			for (const key in bracketsPairs) {
+				if ((stack[stack.length - 1] == key) && (bracketsPairs[key] == bracket[i])) {
+					stack.pop()
+					checkBracket = false
+				}
+			}
+			if (checkBracket) {
+				stack.push(bracket[i])
+			}
+		} else {
+			stack.push(bracket[i])
+		}
+	}
+	return stack.length == 0
 }
